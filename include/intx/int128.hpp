5b9a9a72d6bb83b3a58b69b3e5ab343b701e5d73
fq19851220 <1330747164@qq.com>
2021-08-31 21:47:30 +0800

    Update int128.hpp

--- include/intx/int128.hpp
+++ include/intx/int128.hpp
@@ -16,6 +16,66 @@
 #include <intrin.h>
 #endif
 
+#ifdef __GNUC__
+#   include <x86intrin.h>
+#else
+#   include <intrin.h>
+#   define __restrict__ __restrict
+#endif
+#include <cstdint>
+
+#if defined(__x86_64__) || defined(_M_AMD64)
+#   define EXTRACT64(X) _mm_cvtsi128_si64(X)
+
+#   ifdef __GNUC__
+
+static inline uint64_t __umul128(uint64_t a, uint64_t b, uint64_t* hi)
+{
+    unsigned __int128 r = (unsigned __int128)a * (unsigned __int128)b;
+    *hi = r >> 64;
+    return (uint64_t)r;
+}
+
+#   else
+#define __umul128 _umul128
+#   endif
+#elif defined(__i386__) || defined(_M_IX86)
+#   define HI32(X) \
+    _mm_srli_si128((X), 4)
+
+
+#   define EXTRACT64(X) \
+    ((uint64_t)(uint32_t)_mm_cvtsi128_si32(X) | \
+    ((uint64_t)(uint32_t)_mm_cvtsi128_si32(HI32(X)) << 32))
+
+static inline uint64_t _umul128(uint64_t multiplier, uint64_t multiplicand, uint64_t* product_hi) {
+    // multiplier   = ab = a * 2^32 + b
+    // multiplicand = cd = c * 2^32 + d
+    // ab * cd = a * c * 2^64 + (a * d + b * c) * 2^32 + b * d
+    uint64_t a = multiplier >> 32;
+    uint64_t b = multiplier & 0xFFFFFFFF;
+    uint64_t c = multiplicand >> 32;
+    uint64_t d = multiplicand & 0xFFFFFFFF;
+
+    //uint64_t ac = a * c;
+    uint64_t ad = a * d;
+    //uint64_t bc = b * c;
+    uint64_t bd = b * d;
+
+    uint64_t adbc = ad + (b * c);
+    uint64_t adbc_carry = adbc < ad ? 1 : 0;
+
+    // multiplier * multiplicand = product_hi * 2^64 + product_lo
+    uint64_t product_lo = bd + (adbc << 32);
+    uint64_t product_lo_carry = product_lo < bd ? 1 : 0;
+    *product_hi = (a * c) + (adbc >> 32) + (adbc_carry << 32) + product_lo_carry;
+
+    return product_lo;
+}
+#endif
+
+
+
 namespace intx
 {
 template <unsigned N>
